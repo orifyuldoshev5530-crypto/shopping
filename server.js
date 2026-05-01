6f5634d30d8c58e0db1c7b8bc6ce7357d8a106f0
jason <orifyuldoshev5530@gmail.com>
2026-05-01 17:34:28 +0900

    feat: building simple backend server on Nodejs

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Traditional API
+app.get('/', (req, res) => {
+    res.send(<h1>Hello World!</h1>)
+})
+
+// Rest API
+app.get('/rest', (req, res) => {
+    res.json({ name: "Marco", age: 21, group: MIT })
+})
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${port}`)
+})
